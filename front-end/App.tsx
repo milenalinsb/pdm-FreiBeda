7242385e0dfc3fe03333cfd09695011f19deaa25
Weslley <weslleysouza22@gmail.com>
2023-01-10 22:31:30 -0300

    Integração da tela de login com a api.

--- front-end/App.tsx
+++ front-end/App.tsx
@@ -1,10 +1,13 @@
 import { Box, NativeBaseProvider } from "native-base";
 import { AlertNotificationRoot } from "react-native-alert-notification";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { LogBox } from "react-native";
 import { Routes } from "./Routes";
 import { theme } from "./src/theme";
 
 export default function App() {
+  LogBox.ignoreLogs(["Warning: ..."]);
+  LogBox.ignoreAllLogs();
   return (
     <>
       <GestureHandlerRootView style={{ flex: 1 }}>
