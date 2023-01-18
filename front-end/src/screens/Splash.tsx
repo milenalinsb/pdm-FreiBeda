67f373afe630a603b7ad9c3dc4f124b6841aa5ae
Moacir David <moacirdavidag@gmail.com>
2023-01-18 16:45:51 -0300

    Restaurando navegação inicial para o Login na Splash Screen

--- front-end/src/screens/Splash.tsx
+++ front-end/src/screens/Splash.tsx
@@ -11,7 +11,7 @@ export const Splash = ({ navigation }: NavigationProps) => {
       if (token !== null) {
         navigation.navigate("Dashboard");
       } else {
-        navigation.navigate("PerfilOSCSearch");
+        navigation.navigate("Login");
       }
     }, 100);
     return () => clearTimeout(timer);
