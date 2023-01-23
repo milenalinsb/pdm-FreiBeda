6e493a376ef9e420f8990de2b2160d1359278c15
Weslley <weslleysouza22@gmail.com>
2023-01-22 23:54:01 -0300

    Buscando o avatar no back-end.

--- front-end/src/components/AvatarPerfil/index.tsx
+++ front-end/src/components/AvatarPerfil/index.tsx
@@ -1,30 +1,39 @@
-import { Avatar,Text } from "native-base"
-import { useEffect, useState } from "react"
+import { Avatar, Text } from "native-base";
+import { useEffect, useState } from "react";
+import { baseURL } from "../../services/api";
 import { getUser } from "../../services/asyncStorage";
 import { User } from "../../types/user";
 
 export const AvatarPerfil = () => {
+  const [user, setUser] = useState<User>();
+  const [avatarUrl, setAvatarUrl] = useState(`${baseURL}/static/avatar.jpg`);
+  useEffect(() => {
+    (async () => {
+      const data = (await getUser("@user")) as unknown as User;
+      setUser(JSON.parse(data as unknown as string));
+      if (JSON.parse(data as unknown as string).avatar != null) {
+        setAvatarUrl(
+          `${baseURL}/static/${JSON.parse(data as unknown as string).avatar}`
+        );
+      }
+    })();
+  }, []);
 
-    const [user,setUser] = useState<User>();
-
-      useEffect(()=>{
-        (async () => {
-            const data = await getUser("@user") as unknown as User;
-            setUser(JSON.parse(data as unknown as string))
-        })()
-    },[])
-
-    return(<>
-     <Avatar
-          bg="cyan.500"
-          size="200"
-          source={{
-            uri: "https://wallpapercave.com/wp/wp4923991.png",
-          }}
-        >
-          HS
+  return (
+    <>
+      <Avatar
+        bg="cyan.500"
+        size="200"
+        source={{
+          uri: avatarUrl,
+        }}
+      >
+        HS
       </Avatar>
-      <Text bold fontSize="20">{user?.username} </Text>
+      <Text marginTop={5} bold fontSize="20">
+        {user?.username}{" "}
+      </Text>
       <Text fontSize="15">{user?.email} </Text>
-    </>)
-}
+    </>
+  );
+};
