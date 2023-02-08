68d7f84f321e165f73f5aaec6ebbab5fb1c568d7
Fabíola Diniz <fabioladiniz36@hotmail.com>
2023-02-08 01:43:34 +0000

    ajustes no form

--- front-end/src/components/FormLogin/styles.ts
+++ front-end/src/components/FormLogin/styles.ts
@@ -1,9 +1,24 @@
-import { StyleSheet } from "react-native";
+import { position } from "native-base/lib/typescript/theme/styled-system";
+import { StyleSheet} from "react-native";
 
 export const styles = StyleSheet.create({
     container: {
       flex: 1,
       justifyContent: "center",
     },
+  
+    icon:{
+      width:'15%',
+      height: 50,
+      justifyContent: 'center',
+      marginLeft: '85%',
+      marginBottom:'-15%',
+      position:'absolute'
+      
+    },
+
+    View:{
+      position:'relative'
+    }
   });
   
